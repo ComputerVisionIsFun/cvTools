46bb3898a8afa9e4a9fc99e6d5bd7d5cdc79054e
chiang-en <l18981040@gmail.com>
2021-07-20 21:59:38 +0800

    update

--- cvTools.py
+++ cvTools.py
@@ -6,16 +6,28 @@ cv bgr img is of shape h x w x c
 '''
 
 def _random_crop(bgr, crop_w=24, crop_h=24):
+    if (bgr.shape[0]<=crop_w + 5) or (bgr.shape[0]<=crop_h + 5):
+        bgr = cv2.resize(bgr, (crop_w + 5, crop_h + 5))
+
     random_x = np.random.randint(0, bgr.shape[1] - crop_w)
     random_y = np.random.randint(0, bgr.shape[0] - crop_h)
     return bgr[random_y:random_y+crop_h, random_x:random_x+crop_w, :].copy()
 
 def _random_brightness(bgr):
     hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
-    return 
+    ratio = np.random.randint(10, 50)/20
+    if hsv[:, :, 2].mean()/ratio<=37:
+        return bgr
+    else:
+        return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-def _padding(bgr, pw=1, ph=1):
-    return 
+
+def _random_resize(bgr):
+    r = np.random.randint(2, 5)
+    # print('resize', bgr.shape)
+    output = cv2.resize(bgr, (int(bgr.shape[1]/r), int(bgr.shape[1]/r)))
+    
+    return output
 
 
 
@@ -29,13 +41,19 @@ def random_crop(img_folder='/', save_folder='/', save_header='1_',crop_w = 24, c
         1
 
     for img_name in img_names:
-        print(img_name[-3:])
+        
         if img_name[-3:] in ['png', 'bmp', 'jpg','peg']:
             clean_img_names.append(img_name)
 
     for crop_i in range(num_crop):
         id = np.random.randint(0, len(clean_img_names))
+        # print(clean_img_names[id])
         img = cv2.imread(img_folder + clean_img_names[id], 1)
+        # print('resize', img.shape)
+        if np.random.randint(1, 3)%2==0:
+            img = _random_brightness(img)
+        if np.random.randint(1, 3)%2==0:
+            img = _random_resize(img)
         img_crop = _random_crop(img, crop_w=crop_w, crop_h=crop_h)
         save_path = save_folder + save_header + str(crop_i) + '.png'
         cv2.imwrite(save_path, img_crop)
