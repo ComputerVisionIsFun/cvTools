f74edb77c6e2571af0a92fa41bcfa60f22b866af
chiang-en <71269389+ComputerVisionIsFun@users.noreply.github.com>
2022-02-26 17:44:44 +0800

    update

---
+++ batch_resize.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+
+RESIZE_WIDTH = 24
+RESIZE_HEIGHT = 24
+SAVE_FOLDER = 'D:/Dataset/face/img_align_celeba/24_faces/'
+IMG_FOLDER = 'D:/Dataset/face/img_align_celeba/crop_img_align_celeba/'
+
+filenames = os.listdir(IMG_FOLDER)
+save_i = 0
+for filename in filenames:
+    if filename[-1]=='p' or filename[-1]=='g':
+        img_path = IMG_FOLDER + filename
+        img = cv2.imread(img_path, 0)
+        img = cv2.resize(img, (RESIZE_WIDTH,RESIZE_HEIGHT),1)
+        cv2.imwrite(SAVE_FOLDER + str(save_i) + '.png', img)
+        save_i += 1
+
+
+
---
+++ random_crop.py
@@ -0,0 +1,42 @@
+import cv2
+import random
+import argparse
+import os
+
+
+CROP_WIDTH = 24
+CROP_HEIGHT = 24
+IMG_FOLDER = 'D:/Dataset/Face_detection/bg/'
+NUM_CROP = 100
+CROP_SAVE_FOLDER = 'D:/Dataset/Face_detection/crop_bg/'
+
+
+filenames = os.listdir(IMG_FOLDER)
+crop_save_i = 0
+while(crop_save_i<NUM_CROP):
+
+    ind = random.randint(0, len(filenames) - 1)
+    resize = random.randint(50, 300)/100
+    try:
+        file_path = IMG_FOLDER + filenames[ind]
+        img = cv2.imread(file_path, 0)
+        width, height = img.shape[1], img.shape[0]
+        img = cv2.resize(img, (int(width/resize), int(height/resize)), 1)
+        x, y = random.randint(0, img.shape[1] - CROP_WIDTH - 1), random.randint(0, img.shape[0] - CROP_HEIGHT - 1)
+        crop = img[y:y+CROP_HEIGHT, x:x + CROP_WIDTH]
+        cv2.imwrite(CROP_SAVE_FOLDER + str(crop_save_i) + '.png', crop)
+        crop_save_i+=1
+    except:
+        continue
+
+
+
+
+
+
+
+
+
+
+
+
